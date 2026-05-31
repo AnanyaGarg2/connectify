b421ecd7a212370362cf677c86861f0fa6a6ce1b
Ananya Garg <136894995+AnanyaGarg2@users.noreply.github.com>
2026-06-01 00:08:40 +0530

    Update url.js

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const BACKEND_URL = axios.create({
-    baseURL: 'https://connectify-6ydx.onrender.com'
+    baseURL: 'https://connectify-1-395f.onrender.com'
 });
